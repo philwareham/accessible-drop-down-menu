aa5f40d8cb56394c1f7b14adc9a1b5800ce35103
Phil Wareham <phil@designhive.com>
2014-09-02 11:36:49 +0100

    Wrong grunt plugin name

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt)
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-release');
+    grunt.loadNpmTasks('grunt-tagrelease');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
